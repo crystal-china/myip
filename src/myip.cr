175a921a5b8c5cce7991dff5a7754b6d27d97fde
Billy.Zheng <vil963@gmail.com>
2022-09-17 01:06:19 +0800

    Add exception handle.

--- src/myip.cr
+++ src/myip.cr
@@ -1,19 +1,24 @@
 require "crystagiri"
 
-doc = Crystagiri::HTML.from_url "https://getip.pub"
+begin
+  doc = Crystagiri::HTML.from_url "https://getip.pub"
 
-iframe_urls = [] of String
-iframe = doc.where_tag("iframe") { |tag| iframe_urls << tag.node.attributes["src"].content }
+  iframe_urls = [] of String
+  iframe = doc.where_tag("iframe") { |tag| iframe_urls << tag.node.attributes["src"].content }
 
-iframe_urls.each_with_index do |url, i|
-  ip = Crystagiri::HTML.from_url(url).content
+  iframe_urls.each_with_index do |url, i|
+    ip = Crystagiri::HTML.from_url(url).content
 
-  case i
-  when 0
-    puts ip
-  when 1
-    puts "外网: #{ip}"
-  when 2
-    puts "翻墙: #{ip}"
+    case i
+    when 0
+      puts ip
+    when 1
+      puts "外网: #{ip}"
+    when 2
+      puts "翻墙: #{ip}"
+    end
   end
+rescue OpenSSL::SSL::Error
+  STDERR.puts "Can't visit https://getip.pub"
+  exit
 end
