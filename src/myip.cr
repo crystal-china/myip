f9d2390d98a44713e8a15d8f0df54e48baf70c57
Billy.Zheng <vil963@gmail.com>
2022-09-17 02:57:25 +0800

    User fiber

--- src/myip.cr
+++ src/myip.cr
@@ -1,6 +1,8 @@
 require "crystagiri"
 require "./myip/*"
 
+chan = Channel(String).new
+
 begin
   doc = Crystagiri::HTML.from_url "https://getip.pub"
 
@@ -8,18 +10,24 @@ begin
   iframe = doc.where_tag("iframe") { |tag| iframe_urls << tag.node.attributes["src"].content }
 
   iframe_urls.each_with_index do |url, i|
-    ip = Crystagiri::HTML.from_url(url).content
-
-    case i
-    when 0
-      STDERR.puts ip
-    when 1
-      STDERR.puts "外网 IP：#{ip}"
-    when 2
-      STDERR.puts "翻墙 IP：#{ip}"
+    spawn do
+      chan.send Crystagiri::HTML.from_url(url).content.chomp
     end
   end
 rescue OpenSSL::SSL::Error
   STDERR.puts "Can't visit https://getip.pub"
   exit
+end
+
+3.times do |i|
+  ip = chan.receive
+
+  case i
+  when 0
+    STDERR.puts ip
+  when 1
+    STDERR.puts "外网 IP：#{ip}"
+  when 2
+    STDERR.puts "翻墙 IP：#{ip}"
+  end
 end
