7ea7ae26c52378c10c387c74ab395e1856a9b12a
Billy.Zheng <vil963@gmail.com>
2022-09-17 00:51:03 +0800

    Add myip

--- src/myip.cr
+++ src/myip.cr
@@ -1,6 +1,19 @@
-# TODO: Write documentation for `Myip`
-module Myip
-  VERSION = "0.1.0"
+require "crystagiri"
 
-  # TODO: Put your code here
+doc = Crystagiri::HTML.from_url "https://getip.pub"
+
+iframe_urls = [] of String
+iframe = doc.where_tag("iframe") { |tag| iframe_urls << tag.node.attributes["src"].content }
+
+iframe_urls.each_with_index do |url, i|
+  ip = Crystagiri::HTML.from_url(url).content
+
+  case i
+  when 0
+    puts ip
+  when 1
+    puts "外网: #{ip}"
+  when 2
+    puts "翻墙: #{ip}"
+  end
 end
