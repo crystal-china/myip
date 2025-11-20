fad645a589778816663dd8d6fb87e9a053c76202
Billy.Zheng <vil963@gmail.com>
2025-11-20 22:13:33 +0800

    Fix ip111
    不知道为什么，spawn { ...; chan.send(...) }  无法和 spinner 很好的工作.
    spinner 总是造成 chan.receive 没有等待所有 send 完成，就提前推出，
    考虑完整的移除 spinner.

--- src/myip.cr
+++ src/myip.cr
@@ -19,7 +19,6 @@ class Myip
 
   def ip_from_ifconfig(ip_version : Int32 = 4)
     self.chan_send_count = chan_send_count() + 1
-
     spawn do
       url = "https://ifconfig.io"
       spinner = Term::Spinner.new(":spinner Connecting to #{url.as_title} ...", format: :dots, interval: 0.2.seconds)
@@ -46,7 +45,6 @@ class Myip
 
   def ip_from_ipw(ip_version : Int32 = 4)
     self.chan_send_count = chan_send_count() + 1
-
     spawn do
       url = "http://#{ip_version}.ipw.cn"
       spinner = Term::Spinner.new(":spinner Connecting to #{url.as_title} ...", format: :dots, interval: 0.2.seconds)
@@ -65,7 +63,6 @@ class Myip
 
   def ip_from_ip_sb
     self.chan_send_count = chan_send_count() + 1
-
     spawn do
       url = "https://api.ip.sb/geoip"
       spinner = Term::Spinner.new(":spinner Connecting to #{url.as_title} ...", format: :dots, interval: 0.2.seconds)
@@ -90,55 +87,51 @@ class Myip
 
   def ip_from_ip111
     spinner = Term::Spinner::Multi.new(":spinner", format: :dots, interval: 0.2.seconds)
-
-    # 注意: ip111.cn 仅支持 http, 不支持 https:
-    ip111_url = "http://www.ip111.cn"
-    sp1 = spinner.register ":spinner Connecting to #{ip111_url.as_title} ..."
+    ip111_url = "https://www.ip111.cn"
 
     doc = uninitialized Lexbor::Parser
 
-    sp1.run do
-      doc, _code = from_url(ip111_url)
+    doc, _code = from_url(ip111_url, headers: HTTP::Headers{
+      "User-Agent" => "curl/7.88.1",
+      "Accept"     => "*/*",
+      "Host"       => URI.parse(ip111_url).host.not_nil!,
+    })
 
-      title = doc.css(".card-header").first.tag_text.strip
-      ipinfo = doc.css(".card-body p").first.tag_text.strip
+    title = doc.css(".card-header").first.tag_text.strip
+    ipinfo = doc.css(".card-body p").first.tag_text.strip
 
-      STDERR.puts "#{title}：#{ipinfo}"
-
-      sp1.success
-    end
-
-    headers = HTTP::Headers{"Referer" => "http://www.ip111.cn/"}
+    STDERR.puts "#{title}：#{ipinfo}"
 
     # 这里只能用 each, 没有 map, 因为 doc.nodes("iframe") 是一个 Iterator::SelectIterator 对象
     doc.nodes("iframe").each do |node|
       self.chan_send_count = chan_send_count() + 1
       url = node.attribute_by("src").not_nil!
-      title = node.parent!.parent!.parent!.css(".card-header").first.tag_text.strip
-
-      sp = spinner.register("Connecting to #{url.as_title} ...")
 
       spawn do
-        sp.run do
-          doc, _code = from_url(url, headers: headers)
-          ipinfo = doc.body!.tag_text.strip
+        doc, _code = from_url(
+          url,
+          headers: HTTP::Headers{
+            "Referer" => "http://www.ip111.cn/",
+            "Host"    => URI.parse(url).host.not_nil!,
+          })
+        ipinfo = doc.body!.tag_text.strip
+        # ip = ipinfo[/[a-z0-9:.]+/]
 
-          ip = ipinfo[/[a-z0-9:.]+/]
-
-          chan.send({"#{title}：#{ipinfo}", ip})
-
-          sp.success
-        rescue ex : ArgumentError | Socket::Error
-          chan.send({ex.message.not_nil!, nil})
-        end
+        # 这里的 parse title 涉及一些 IO 等待情况（非一蹴而就）
+        # 如果在 spawn 外面解析 title, 然后传递 title 到 spawn 代码块里面，
+        # 此时会涉及 "共享变量" 的经典问题，即： spawn 内部共享外面的变量
+        # 可能会出现，spawn 内部看到的外部的 url 是两个相同的 url.
+        title = node.parent!.parent!.parent!.css(".card-header").first.tag_text.strip
+        chan.send({"#{title}：", ipinfo})
+      rescue ex : ArgumentError | Socket::Error | OpenSSL::SSL::Error
+        chan.send({ex.message.not_nil!, nil})
       end
     end
   end
 
   def ip_from_ip138
+    self.chan_send_count = chan_send_count() + 1
     spinner = Term::Spinner::Multi.new(":spinner", format: :dots, interval: 0.2.seconds)
-    self.chan_send_count = chan_send_count + 1
-
     spawn do
       url = "https://www.ip138.com"
       sp = spinner.register("Connecting to #{url.as_title} ...")
@@ -198,42 +191,53 @@ class Myip
       select
       when value = chan.receive
         ipinfo, ip = value
-
-        STDERR.puts "\n#{ipinfo}"
-
-        if !ip.nil?
-          self.detail_chan_send_count = detail_chan_send_count() + 1
-          details_ip_url = "https://www.ipshudi.com/#{ip}.htm"
-          sp = spinner.register("Connecting to #{details_ip_url.as_title} ...")
-
-          spawn do
-            sp.run do
-              doc, _code = from_url(details_ip_url)
-
-              output = String.build do |io|
-                doc.css("div.ft>table>tbody>tr>td").each do |x|
-                  io << x.tag_text
-                end
-              end
-
-              detail_chan.send(output.squeeze('\n'))
-
-              sp.success
-            end
-          end
-        end
-      when timeout 5.seconds
+        STDERR.puts "#{ipinfo}: #{ip}"
+      when timeout 10.seconds
         STDERR.puts "Timeout, check your network connection!"
         exit
       end
     end
 
-    detail_chan_send_count.times do
-      select
-      when ipinfo = detail_chan.receive
-        STDERR.puts ipinfo
-      end
-    end
+    # chan_send_count.times do
+    #   select
+    #   when value = chan.receive
+    #     ipinfo, ip = value
+
+    #     STDERR.puts "\n#{ipinfo}"
+
+    #     # if !ip.nil?
+    #     #   self.detail_chan_send_count = detail_chan_send_count() + 1
+    #     #   details_ip_url = "https://www.ipshudi.com/#{ip}.htm"
+    #     #   sp = spinner.register("Connecting to #{details_ip_url.as_title} ...")
+
+    #     #   spawn do
+    #     #     sp.run do
+    #     #       doc, _code = from_url(details_ip_url)
+
+    #     #       output = String.build do |io|
+    #     #         doc.css("div.ft>table>tbody>tr>td").each do |x|
+    #     #           io << x.tag_text
+    #     #         end
+    #     #       end
+
+    #     #       detail_chan.send(output.squeeze('\n'))
+
+    #     #       sp.success
+    #     #     end
+    #     #   end
+    #     # end
+    #   when timeout 5.seconds
+    #     STDERR.puts "Timeout, check your network connection!"
+    #     exit
+    #   end
+    # end
+
+    # detail_chan_send_count.times do
+    #   select
+    #   when ipinfo = detail_chan.receive
+    #     STDERR.puts ipinfo
+    #   end
+    # end
   end
 
   private def from_url(url : String, *, follow : Bool = false, headers = HTTP::Headers.new) : Tuple(Lexbor::Parser, Int32)
