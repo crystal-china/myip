451d0349685b1337674f4e788358c996ced1f2d2
Billy.Zheng <vil963@gmail.com>
2026-09-18 16:03:44 +0800

    added spec for spinner

--- spec/myip_spec.cr
+++ spec/myip_spec.cr
@@ -4,4 +4,14 @@ describe Myip do
   it "works" do
     false.should eq(false)
   end
+
+  it "starts a spinner on Crystal's current execution context" do
+    spinner = Term::Spinner.new(interval: 1.millisecond)
+
+    spinner.run do
+      spinner.success
+    end
+
+    spinner.done?.should be_true
+  end
 end
